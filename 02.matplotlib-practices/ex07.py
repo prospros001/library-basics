27b1eefb89c9d3e89a8f3207748eeabfe9d199aa
prospros001 <prospros@naver.com>
2020-11-16 16:28:22 +0900

    matplotlib 연습#2

--- 02.matplotlib-practices/ex07.py
+++ 02.matplotlib-practices/ex07.py
@@ -6,11 +6,14 @@ subplot() 함수 사용
 """
 
 from matplotlib import pyplot as plt
+from numpy.random import randn
+import numpy as np
 
-fig, splits = plt.subplots(2, 2)
-sp[0,0].plot([2, 3, 4, 5], [81, 93, 91, 97])
-sp2[0, 1].plot(randn(50).cumsum(), 'b--')
-sp3[1,0].hist(randn(100), color='k', alpha=0.3)
-sp4[1,1].scatter(np.arange(100), np.arange(100) + 3 * randn(100))
+fig, sp = plt.subplots(2, 2)
+
+sp[0, 0].plot([2, 3, 4, 5], [81, 93, 91, 97])
+sp[0, 1].plot(randn(50).cumsum(), 'b--')
+sp[1,0].hist(randn(100), color='k', alpha=0.3)
+sp[1,1].scatter(np.arange(100), np.arange(100) + 3 * randn(100))
 
 plt.show()
