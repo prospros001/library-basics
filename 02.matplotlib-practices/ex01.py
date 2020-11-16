23ee67c04db182abd73663dd096dfd59d67038a2
prospros001 <prospros@naver.com>
2020-11-16 15:17:23 +0900

    matplotlib 연습#1

---
+++ 02.matplotlib-practices/ex01.py
@@ -0,0 +1,12 @@
+# 기본 사용법
+
+"""
+plot() 함수 사용해서 그래프 그린다.
+첫번째 인자 : x 축 값들
+두번째 인자 : y 축 값들
+"""
+from matplotlib import pyplot as plt
+
+plt.plot([2, 4, 5, 6], [81, 93, 91, 97])
+plt.show()
+
---
+++ 02.matplotlib-practices/ex02.py
@@ -0,0 +1,12 @@
+# 기본 사용법
+"""
+
+y 축 값만 할당 가능하다. x 값은 내부에서 자동으로 0부터 매칭한다.
+
+
+"""
+
+from matplotlib import pyplot as plt
+
+plt.plot([10, 20, 30, 40])
+plt.show()
